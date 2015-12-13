b41fc35db21f4c3be530d7a4c32481e7e8ed681b
Nemeros <a.bilbaut@free.fr>
2015-12-13 11:19:36 +0100

    csrf protection

--- src/main/java/com/nemeros/Security.java
+++ src/main/java/com/nemeros/Security.java
@@ -6,6 +6,9 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -13,25 +16,34 @@ public class Security extends WebSecurityConfigurerAdapter {
 	
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        //http.csrf().disable();
 		http
             .authorizeRequests()
             .antMatchers("/webjars/**","/static/**")
             .permitAll()
             .anyRequest().authenticated()
-                .and()
+            .and()
             .formLogin()
                 .loginPage("/login")
                 .passwordParameter("password")
                 .usernameParameter("user")
                 .defaultSuccessUrl("/greeting")
                 .loginProcessingUrl("/loginProcess")
-                .permitAll();
+                .permitAll()
+            .and()
+            .addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class);
+		http.csrf().csrfTokenRepository(csrfTokenRepository());
     }
 	
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
                    .withUser("user").password("123").roles("USER");
+	}
+	
+	private CsrfTokenRepository csrfTokenRepository() {
+	  HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+	  repository.setHeaderName("X-XSRF-TOKEN");
+	  return repository;
 	}
 }
